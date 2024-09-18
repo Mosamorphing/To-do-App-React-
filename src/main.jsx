0f35a97ba57f9a0663f3605509c11c95f4e600a3
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-09-18 16:50:12 +0100

    sorted out the most important components and now, i've created a 'form' and 'filterbutton' file using 'New-Item src/components/Form.jsx -ItemType File' 'New-Item src/components/FilterButton.jsx -ItemType File'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
+// Define your DATA array
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Code", completed: false },
+  { id: "todo-2", name: "Sleep", completed: false },
+];
+
+// Get the root DOM element and render the App component
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <App tasks={DATA} />  {/* Pass tasks as a prop here */}
+  </StrictMode>
+);
